81217bdd68429eeac855a5e9fe49ab5aac85e935
mparedes003 <onnabugeishadev@gmail.com>
2019-04-08 14:30:20 -0700

    Add RAM functions cpu_ram_read and cpu_ram_write.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,34 @@
 #include "cpu.h"
+#include <stdio.h>
 
 #define DATA_LEN 6
+
+// Add functions cpu_ram_read() and cpu_ram_write()
+// that access the RAM inside the struct cpu
+
+// Read function reads from ram, it returns an unsigned char,
+// based on how it is defined in struct cpu:
+
+// Accepts 2 arguments
+// A pointer to the cpu and an index in ram[] to access
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
+{
+
+  // Return the data held in ram at the specified index
+  return cpu->ram[index];
+}
+
+// Write function no need to return anything specific
+// It can have the type of void
+
+// Accepts 3 arguments
+// A pointer to the cpu, an index to be written to, and a value to write
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value)
+{
+
+  // Assign the specified value to the value at specified index
+  cpu->ram[index] = value;
+}
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -8,18 +36,19 @@
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,10 +60,11 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
@@ -47,7 +77,8 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. Figure out how many operands this next instruction requires
